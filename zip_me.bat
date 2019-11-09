55358217b7f6f4454a4c4cf2a138efab86388b10
aey76 <aey76>
2019-11-09 22:42:52 +0200

    Remove images folder from zip

--- zip_me.bat
+++ zip_me.bat
@@ -1,2 +1,2 @@
 
-"C:\Program Files\7-Zip\7z.exe" a -r -xr!darknet53.conv.74 -xr!*.weights yoloGui_Code_.7z *.*
+"C:\Program Files\7-Zip\7z.exe" a -r -xr!images -xr!darknet53.conv.74 -xr!*.weights yoloGui_Code_.7z *.*
